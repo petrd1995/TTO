d5549f2cdd3d05c526aa175e220863abcf941efb
Petr David <david.petr7@gmail.com>
2021-06-06 17:42:41 +0200

    cleaned up testing file

--- testing.py
+++ testing.py
@@ -4,25 +4,40 @@
 
 import TTO
 import numpy as np
+import emailnotify
 
-example = TTO.Truss('example', 100, 100, 0, 2, 2, 2, np.array([[0,1,1,1],[1,1,1,1]]), np.array([[2,0,100, 0]]), 2.1e5, 10, 0, 0.2, 1, 1)
+x0 = 100
+y0 = 100
+z0 = 100
+nx = 2
+ny = 2
+nz = 2
+E = 2.1e5
+r0 = 5
+ratio = 0.5
+Vol0 = x0*y0*z0
+Ro = 1
+kon = 1
+bcs = np.array([[0, 1, 1, 1], [1, 1, 1, 1], [2, 1, 1, 1], [3, 1, 1, 1]])
+f = np.array([[5, 0, -1000, -600]])
 
-example.default_setup()
+nodes = np.array([[0, 0, 0],
+                  [10, 0, 0],
+                  [0, 10, 0],
+                  [0, 0, 10],
+                  [0, 30, 30],
+                  [50, 0, 50],
+                  [50, 20, 50],
+                  [50, 50, 50]])
+
+
+example = TTO.Truss('example', x0, y0, z0, nx, ny, nz,
+                    bcs, f, E, r0, Vol0, ratio, Ro, kon)
+
+example.grid_from_list(nodes)
+example.create_bars()
+example.vec_len()
 example.opt()
-
-
-# example.Avec = np.ones_like(example.len) * example.A0
-
-# example.MK()
-# example.forces()
-# K0 = example.K
-# example.boundary()
-# K1 = example.Ksys
-# print(K0)
-# print(K1)
-
-# F = example.f
-# Fsys = example.fsys
-
-# u = Fsys
-# u[:,1] = np.linalg.inv(K1) @ Fsys[:,1]
+example.plot('res')
+# example.plot('bcs')
+# example.plot('grid')
